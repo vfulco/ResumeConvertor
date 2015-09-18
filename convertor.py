e72e2aba7c6349e37130ff49316639e80866b62b
SirEdvin <siredvin.dark@gmail.com>
2015-09-18 11:51:12 +0300

    Add latex support

--- convertor.py
+++ convertor.py
@@ -6,12 +6,14 @@ import argparse
 import json
 from functools import reduce
 import markdowncovert as md
+import latexconvert as tex
 from jinja2 import Environment, FileSystemLoader
 import os
 
 parser = argparse.ArgumentParser(description='Tool to convert json resume to markdown and latex format')
 parser.add_argument("file", type=str, help="path to json resume file")
-parser.add_argument("--template", type=str, default="markdown.template", help="custom template based on jinja2")
+parser.add_argument("--template-directory", type=str, default="templates",
+                    help="template directory with markdown and latex templates")
 parser.add_argument("--localization-directory", type=str, default="locale",
                     help="directory with localization python files")
 parser.add_argument("--output", type=str, default="resume_%", help="Output file template % will be replaced by locale")
@@ -24,21 +26,21 @@ resume = json.loads(reduce(lambda x, y: x + y, resumeFile.readlines(), ""))
 resumeFile.close()
 locales = resume['localizations']
 en_json_markdown = md.generate_localized_markdown(resume, 'en')
+en_json_latex = tex.generate_localized_latex(resume, 'en')
 local_file = open(os.path.join(args.localization_directory, "en.py"))
 exec(util.read_full_file(local_file))
 local_file.close()
-print(localization_map) #inject from en.py
+print(localization_map)  # inject from en.py
 en_json_markdown.update(localization_map)
+en_json_latex.update(localization_map)
 # print(json.dumps(en_json_markdown, sort_keys=True, indent=4, separators=(',', ': ')))
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-markdown_en = open(args.output.replace('%', 'en')+".md", 'w')
-j2_env = Environment(loader=FileSystemLoader(THIS_DIR), trim_blocks=True)
+markdown_en = open(args.output.replace('%', 'en') + ".md", 'w')
+j2_env = Environment(loader=FileSystemLoader(args.template_directory), trim_blocks=True)
 markdown_en.write(j2_env.get_template('markdown.template').render(en_json_markdown))
 markdown_en.close()
-
-
-
-
-
+latex_en = open(args.output.replace('%', 'en')+'.tex', 'w')
+latex_en.write(j2_env.get_or_select_template('latex.template').render(en_json_latex))
+latex_en.close()
--- latexconvert.py
+++ latexconvert.py
@@ -4,34 +4,34 @@ __author__ = 'siredvin'
 
 import util
 
-def markdown_parse_link(link_node):
-    return "["+link_node['text']+']('+link_node['link']+')'
+def latex_parse_link(link_node):
+    return "\\href{"+link_node['link']+'}{'+link_node['text']+'}'
 
-def generate_localized_markdown(resume, locale):
+def generate_localized_latex(resume, locale):
     resume = deepcopy(resume)
     localizations = resume['localizations']
     resume.pop('localizations', None)
-    return generate_markdown_part(resume, locale, localizations)
+    return generate_latex_part(resume, locale, localizations)
 
-def generate_markdown_part(resume, locale, localizations):
+def generate_latex_part(resume, locale, localizations):
     for key in resume.keys():
-        resume[key] = parse_markdown_part(resume[key], locale, localizations)
+        resume[key] = parse_latex_part(resume[key], locale, localizations)
     return resume
 
-def parse_markdown_part(node, locale, localizations):
+def parse_latex_part(node, locale, localizations):
     if isinstance(node, dict):
-        node = generate_markdown_part(node, locale, localizations)
+        node = generate_latex_part(node, locale, localizations)
     if isinstance(node, list):
-        node = list(map(lambda x: parse_markdown_part(x, locale, localizations), node))
+        node = list(map(lambda x: parse_latex_part(x, locale, localizations), node))
     if util.link_check(node):
-        node = markdown_parse_link(node)
+        node = latex_parse_link(node)
     if util.localized_check(node, localizations):
         node = node[locale]
     elif util.text_flow_check(node):
         generated_text = ''
         for text_element in node["text_flow"]:
             if util.link_check(text_element):
-                generated_text += markdown_parse_link(text_element)
+                generated_text += latex_parse_link(text_element)
             else:
                 generated_text += text_element
         node = generated_text
